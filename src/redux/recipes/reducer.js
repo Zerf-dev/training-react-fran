6bc0e3d37db39ece07532ee051ee0f64986cd998
josibuttazzoni <mariajosebuttazzoni@hotmail.com>
2023-05-18 16:12:14 -0300

    Add intiial state

--- src/redux/recipes/reducer.js
+++ src/redux/recipes/reducer.js
@@ -3,11 +3,15 @@ import { createReducer, onReadValue } from "redux-recompose";
 
 import { actions } from "./actions";
 
+const initialState = {
+  favourite: [],
+};
+
 const reducerDescription = {
   [actions.ADD_FAVOURITE]: onReadValue(),
   [actions.REMOVE_FAVOURITE]: onReadValue(),
 };
 
-const reducer = createReducer(new Immutable({}), reducerDescription);
+const reducer = createReducer(new Immutable(initialState), reducerDescription);
 
 export default reducer;
