5057a0da8979883b9e1c10ba643fc3bcb91944ef
josibuttazzoni <mariajosebuttazzoni@hotmail.com>
2023-05-18 16:15:41 -0300

    Fix destructure recipe

--- src/redux/recipes/actions.js
+++ src/redux/recipes/actions.js
@@ -9,29 +9,25 @@ export const actions = createTypes(
 );
 
 export const actionCreators = {
-  addFavourite:
-    ({ recipe }) =>
-    (dispatch, getState) => {
-      const favourites = getState().recipes.favourite;
+  addFavourite: (recipe) => (dispatch, getState) => {
+    const favourites = getState().recipes.favourite;
 
-      dispatch({
-        type: actions.ADD_FAVOURITE,
-        target: "favourite",
-        payload: [...favourites, recipe],
-      });
-    },
+    dispatch({
+      type: actions.ADD_FAVOURITE,
+      target: "favourite",
+      payload: [...favourites, recipe],
+    });
+  },
 
-  removeFavourite:
-    ({ recipe }) =>
-    (dispatch, getState) => {
-      const favourites = getState().recipes.favourite;
+  removeFavourite: (recipe) => (dispatch, getState) => {
+    const favourites = getState().recipes.favourite;
 
-      dispatch({
-        type: actions.REMOVE_FAVOURITE,
-        target: "favourite",
-        payload: favourites.filter((favourite) => favourite.id !== recipe.id),
-      });
-    },
+    dispatch({
+      type: actions.REMOVE_FAVOURITE,
+      target: "favourite",
+      payload: favourites.filter((favourite) => favourite.id !== recipe.id),
+    });
+  },
 };
 
 export default actionCreators;
